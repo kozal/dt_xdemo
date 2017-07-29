7a0e3414a99d8733e02657d599bf0b1a24180914
aok <omer.kozal@metu.edu.tr>
2017-07-29 16:49:33 +0300

    inc folder added

---
+++ xdemo/inc/diag2.h
@@ -0,0 +1,11 @@
+// #include "../include/xcommand.h"
+#include <stdio.h>
+#include <iostream>
+
+#ifndef DIAG2
+#define DIAG2
+
+void DisplayMenu();
+void DisplayMenu2();
+void handle_error_msg(uint32_t err_id);
+#endif //DIAG2
